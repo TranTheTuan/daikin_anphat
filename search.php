af6d3b444e81c0dceff1b4122516ef96a9134934
TranTheTuan <tranthetuan1998hanam@gmail.com>
2021-05-04 00:22:54 +0700

    init commit

---
+++ search.php
@@ -0,0 +1,11 @@
+/*
+ * Theme Name: Daikin Anphat Theme v1.1
+ * Description: Theme for Anphat company - Daikin's distributor in Vietnam
+ * Version: 1.1
+ * Theme Author: Tran The Tuan, Tran Minh Hieu
+ * Tags: starter theme, framework theme, two-columns, customize
+ * Textdomain: daikin_anphat
+ */
+
+
+

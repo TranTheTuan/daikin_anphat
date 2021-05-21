07c68aacce3b9d312e7e1358976dca7cc43f9de1
tuantt <tuantt@smartosc.com>
2021-05-21 19:05:15 +0700

    add new update

--- single-product_line.php
+++ single-product_line.php
@@ -22,68 +22,59 @@ get_header();
         <?php endif ?>
         </div>
         <ul class="spec-headers">
-        <?php if (count(CFS()->get('features')) > 0): ?>
+        <?php if (CFS()->get('features') != ""): ?>
             <li><a href="#ft-block">Features</a></li>
         <?php endif ?>
-        <?php if (count(CFS()->get('functions')) > 0): ?>
+        <?php if (CFS()->get('functions') != ""): ?>
             <li><a href="#func-block">Functions</a></li>
         <?php endif ?>
-        <?php if (count(CFS()->get('function_options')) > 0): ?>
+        <?php if (CFS()->get('function_options') != ""): ?>
             <li><a href=#fo-block">Function Options</a></li>
         <?php endif ?>
         <?php if (CFS()->get('unit_options') != ""): ?>
             <li><a href="#uo-block">Unit Options</a></li>
         <?php endif ?>
-        <?php if (count(CFS()->get('hardware_options')) > 0): ?>
+        <?php if (CFS()->get('hardware_options') != ""): ?>
             <li><a href="#ho-block">Hardware Options</a></li>
         <?php endif ?>
-        <?php if (count(CFS()->get('case_studies')) > 0): ?>
+        <?php if (CFS()->get('case_studies') != ""): ?>
             <li><a href="#cs-block">Case Study of energy-saving</a></li>
         <?php endif ?>
         </ul>
     </div>
 
     <div class="dk-body w-75 mx-auto">
-        <?php if (count(CFS()->get('features')) > 0): ?>
-        <div id="ft-block">
-            <h3>Features</h3>
-            <?php foreach(CFS()->get('features') as $key => $ft): ?>
-                <div><?php echo $ft['ft_title']; ?></div>
-                <div><?php echo $ft['ft_description']; ?></div>
-                <div><?php echo $ft['ft_detail']; ?></div>
-            <?php endforeach; ?>
-        </div>
+        <?php if (CFS()->get('features') != ""):?>
+            <?php get_template_part('/template-parts/product_line', 'features', array('attrs' => CFS()->get('features'), 'id' => 'ft-block', 'name' => 'Features')); ?>
         <?php endif; ?>
 
-        <?php if (count(CFS()->get('functions')) > 0): ?>
-        <div id="func-block">
-            <h3>Functions</h3>
-            <?php foreach(CFS()->get('functions') as $ft): ?>
-                <div><?php echo $ft['func_title']; ?></div>
-                <div><?php echo $ft['func_description']; ?></div>
-                <div><?php echo $ft['func_detail']; ?></div>
-            <?php endforeach; ?>
-        </div>
+        <?php if (CFS()->get('functions') != ""):?>
+            <?php get_template_part('/template-parts/product_line', 'features', array('attrs' => CFS()->get('functions'), 'id' => 'func-block', 'name' => 'Functions')); ?>
         <?php endif; ?>
 
-        <?php if (count(CFS()->get('function_options')) > 0): ?>
-        <div id="fo-block">
-            <h3>Functions Options</h3>
-            <?php foreach(CFS()->get('function_options') as $ft): ?>
-                <div><?php echo $ft['fo_title']; ?></div>
-                <div><?php echo $ft['fo_description']; ?></div>
-                <div><?php echo $ft['fo_detail']; ?></div>
-            <?php endforeach; ?>
-        </div>
+        <?php if (CFS()->get('function_options') != ""):?>
+            <?php get_template_part('/template-parts/product_line', 'features', array('attrs' => CFS()->get('function_options'), 'id' => 'fo-block', 'name' => 'Function Options')); ?>
         <?php endif; ?>
 
-        <?php if (count(CFS()->get('hardware_options')) > 0): ?>
-        <div id="ho-block">
-            <h3>Hardware Options</h3>
-            <?php foreach(CFS()->get('hardware_options') as $ft): ?>
-                <div><?php echo $ft['ho_name']; ?></div>
-            <?php endforeach; ?>
-        </div>
+        <?php if (CFS()->get('hardware_options') != ""): ?>
+            <div id="ho-block">
+                <h3>Hardware Options</h3>
+                <?php foreach(CFS()->get('hardware_options') as $ft): ?>
+                    <div><?php echo $ft['ho_name']; ?></div>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (CFS()->get('case_studies') != ""): ?>
+            <div id="ho-block">
+                <h3>Case studies of energy-saving</h3>
+                <div>
+                    <h4>Various case studies of energy-saving such as the power consumption reductions in 72%.</h4>
+                    <?php foreach(CFS()->get('case_studies') as $ft): ?>
+                        <div><a href="<?php echo $ft['link'] ?>"><?php echo $ft['name']; ?></a></div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
         <?php endif; ?>
     </div>
 </div>

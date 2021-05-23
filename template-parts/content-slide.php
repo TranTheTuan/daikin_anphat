8817031a7292585f1ff1e2a234e5d5073fa723ff
TranTheTuan <tranthetuan1998hanam@gmail.com>
2021-05-24 00:34:50 +0700

    update front

--- template-parts/content-slide.php
+++ template-parts/content-slide.php
@@ -1,5 +1,5 @@
-<div class="container">
-    <div class="w-75 mx-auto">
+<div class="container main-content">
+    <div class="mx-auto">
         <div id="carouselExampleIndicators" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -7,13 +7,13 @@
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active" style="height: 400px;">
+                <div class="carousel-item active" style="height: 390px;">
                     <img style="max-width: 100%; max-height: 100%;" src="https://khamphacungpetronas.com.vn/wp-content/uploads/2019/01/he-thong-thuy-luc.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item" style="height: 400px;">
+                <div class="carousel-item" style="height: 390px;">
                     <img style="max-width: 100%; max-height: 100%;" src="https://anduongviet.com/wp-content/uploads/2020/08/hydraulic-cylinder.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item" style="height: 400px;">
+                <div class="carousel-item" style="height: 390px;">
                     <img style="max-width: 100%; max-height: 100%;" src="https://yenhung.vn/contents_yenhung/images/he-thong-thuy-luc-2(1).jpg" class="d-block w-100" alt="...">
                 </div>
             </div>

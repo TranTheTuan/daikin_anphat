49cf96bbf982492d93a81588bc1926322e9a1886
TMH <hieumt2198@outlook.com>
2021-06-09 01:39:49 +0700

    update partners, colors

--- front-page.php
+++ front-page.php
@@ -2,4 +2,5 @@
 <?php get_template_part('/template-parts/content', 'slide') ?>
 <?php get_template_part('/template-parts/product_line', 'list') ?>
 <?php get_template_part('/template-parts/contents', 'list') ?>
+<?php get_template_part('/template-parts/partners', 'list') ?>
 <?php get_footer(); ?>

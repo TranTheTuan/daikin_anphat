9a421d5bb3a9efe1e7ae2ec701fdae4501f3ca56
TranTheTuan <tranthetuan1998hanam@gmail.com>
2021-12-02 01:11:06 +0700

    update slide images

--- template-parts/content-slide.php
+++ template-parts/content-slide.php
@@ -4,17 +4,21 @@
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>`
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active" style="height: 390px;">
-                    <img style="max-width: 100%; max-height: 100%;" src="https://khamphacungpetronas.com.vn/wp-content/uploads/2019/01/he-thong-thuy-luc.jpg" class="d-block w-100" alt="...">
+                    <img style="max-width: 100%; max-height: 100%;" src="<?php echo get_template_directory_uri(); ?>/assets/images/slides/i1.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item" style="height: 390px;">
-                    <img style="max-width: 100%; max-height: 100%;" src="https://anduongviet.com/wp-content/uploads/2020/08/hydraulic-cylinder.jpg" class="d-block w-100" alt="...">
+                    <img style="max-width: 100%; max-height: 100%;" src="<?php echo get_template_directory_uri(); ?>/assets/images/slides/i2.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item" style="height: 390px;">
-                    <img style="max-width: 100%; max-height: 100%;" src="https://yenhung.vn/contents_yenhung/images/he-thong-thuy-luc-2(1).jpg" class="d-block w-100" alt="...">
+                    <img style="max-width: 100%; max-height: 100%;" src="<?php echo get_template_directory_uri(); ?>/assets/images/slides/i3.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" style="height: 390px;">
+                    <img style="max-width: 100%; max-height: auto;" src="<?php echo get_template_directory_uri(); ?>/assets/images/slides/i4.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
